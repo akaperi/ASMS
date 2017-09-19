95ddd07eb5a244d7b4f4dcc911e9b2caa5118967
Arjun GH <MallikarjunGHagargi@github.com>
2017-09-19 14:19:21 +0530

    Demo project initial commit

---
+++ Demo/src/First.java
@@ -0,0 +1,18 @@
+/**
+@{author} mallikarjun.guranna
+19-Sep-2017
+*/
+public class First {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hi");
+	}
+	/**
+	@{author} mallikarjun.guranna
+	19-Sep-2017
+	*/
+}
