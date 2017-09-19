773dbff6c0c6d66760bbaed398d6a5fd917970cd
Arjun GH <MallikarjunGHagargi@github.com>
2017-09-19 14:27:26 +0530

    updated code

--- Demo/src/First.java
+++ Demo/src/First.java
@@ -4,15 +4,9 @@
 */
 public class First {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Hi");
 	}
-	/**
-	@{author} mallikarjun.guranna
-	19-Sep-2017
-	*/
+	
 }
